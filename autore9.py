3f60280143e76eeda8bf0895650d9edb8e412e2b
Tiago Sutter <tiagosutter@gmail.com>
2017-05-02 13:00:38 -0300

    Cria a função consulta

--- autore9.py
+++ autore9.py
@@ -1,6 +1,15 @@
 import urllib.request
 import urllib.parse
 
+
+def consulta(rgu, senha, url):
+    """Faz a consulta e retorna o resultado html em uma string."""
+    login_payload = dict(codinterno=rgu, senha=senha)
+    encoded_login_payload = urllib.parse.urlencode(login_payload).encode()
+    with urllib.request.urlopen(url, data=encoded_login_payload) as resposta:
+        html = resposta.read().decode("iso-8859-1")
+    return html
+
 url_base = 'http://200.20.252.54/informaweb/cgi-bin/iwmoduloleitor.dll/empcons?'
 
 # dados necessários para fazer consulta de emprestimos
