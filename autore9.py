bada41c4af5fcee4fa9e30e53b31dca5339ff021
Tiago Sutter <tiagosutter@gmail.com>
2017-05-01 22:00:51 -0300

    Adiciona dados e lógica de criação de url base A url base será utilizada para consultar e renovar as publicações emprestadas. Optei pelos dados em um dicionário e a sua codificação posterior para tornar o código mais legível e fácil de alterar, caso haja necessidade de mudanças.

---
+++ autore9.py
@@ -0,0 +1,17 @@
+import urllib.request
+import urllib.parse
+
+url_base = 'http://200.20.252.54/informaweb/cgi-bin/iwmoduloleitor.dll/empcons?'
+
+# dados necessários para fazer consulta de emprestimos
+query = {'bdbanco': 'InformaUCP',
+         'g': 'web',
+         'grupo': '***',
+         'idSessao': '{C2B12B23-57F6-4FD9-A6CA-71D75901960A}',
+         'idioma': 'POR',
+         'rotina': 'EMP',
+         'tipocons': 'EMPRESTIMO',
+         'unidade': ''}
+
+query_string = urllib.parse.urlencode(query)
+url_consulta = url_base + query_string
